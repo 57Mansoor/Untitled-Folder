c09c1adbf3579022041fdf34870cb12c7d5dc6e1
57Mansoor <skmansoorintership@gmail.com>
2025-01-01 00:56:15 +0530

    this

---
+++ increment/script.js
@@ -0,0 +1,11 @@
+function Increment(){
+    let value=document.getElementById("head").innerHTML
+    value++
+    document.getElementById('head').innerHTML=value
+}
+
+function Decrement(){
+    let value=document.getElementById("head").innerHTML
+    value--
+    document.getElementById('head').innerHTML=value
+}
---
+++ rain/script.js
@@ -0,0 +1,8 @@
+function rainon(){
+    let img1=document.getElementById('img')
+    img1.src="rain.png"
+}
+function rainoff(){
+    let img2=document.getElementById('img')
+    img2.src="sky.png"
+}       
